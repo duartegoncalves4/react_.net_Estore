6a30cd050fc082bb93e51f948a5aa7f98a610a64
Duarte Rui Goncalves <duagoncalves@deloitte.pt>
2025-01-09 14:19:32 +0000

    first push

---
+++ API/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace API.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Desctription { get; set; }
+        public long Price { get; set; }
+        public required string PictureUrl { get; set; }
+        public required string Type { get; set; }
+        public required string Brand { get; set; }
+        public int QuantityStock { get; set; }
+    }
+}
